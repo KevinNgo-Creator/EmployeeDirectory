333290c57fdfc791e74e1b165b0c9d348090e757
Kevin Ngo <ltn8rd@mail.umkc.edu>
2020-04-24 10:58:02 -0500

    push to repo

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Navbar(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <li className="navbar-item" href="/">
+            Employee Directory
+            </li>
+        </nav>
+        );
+    }
+
+export default Navbar;
